d5d4a61c9d777077b85651c52755a98501ef7aa9
Kevin Agyeman <giromoney@yahoo.com>
2019-04-10 11:58:08 +0100

    graph download files

---
+++ session_10042019.R
@@ -0,0 +1,8 @@
+
+
+dir.create('data')
+
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder_data.csv", destfile = "data/gapminder_data.csv")
+gapminder <- read.csv("data/gapminder_data.csv")
+
+
